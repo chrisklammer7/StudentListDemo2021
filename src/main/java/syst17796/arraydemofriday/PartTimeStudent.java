5a6e7eeddc52e0c763e3dc8516b4f0d3850cba27
Chris <Chris@192.168.2.227>
2021-05-28 11:49:43 -0400

    part time student

---
+++ src/main/java/syst17796/arraydemofriday/PartTimeStudent.java
@@ -0,0 +1,13 @@
+package syst17796.arraydemofriday;
+/**
+ * This class for a PART TIME student.
+ *
+ * @author Chris Klammer. "In the beginning the Universe was created.
+ * This has made a lot of people very angry and has been widely regarded as a
+ * bad move." - Douglas Adams (The Restaurant at the End of the Universe, 1980).
+ *
+ */
+public class PartTimeStudent
+{
+
+}
